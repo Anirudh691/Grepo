216d1845949ca740d395d20630b2d846f3ed4e7a
ANIRUDHA <ANIRUDHA@LAPTOP-RV1DLD35>
2022-07-06 20:40:02 +0530

    push and commit

---
+++ project_4/src/pkg1/Student1.java
@@ -0,0 +1,14 @@
+package pkg1;
+
+public class Student1 {
+	public void anirudha()
+	{
+		System.out.println("New task");
+	}
+	public static void main(String[] args) 
+	{
+		Student1 abc=new Student1();
+		abc.anirudha();
+	}
+
+}
